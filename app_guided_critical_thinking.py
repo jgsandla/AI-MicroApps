977ee6dd3dc335ce96c8cccfeaa733593b3c1e71
jgsandla <46978608+jgsandla@users.noreply.github.com>
2024-10-30 16:05:16 -0400

    Update app_guided_critical_thinking.py

--- app_guided_critical_thinking.py
+++ app_guided_critical_thinking.py
@@ -17,7 +17,7 @@ SHARED_ASSET = {
 }
 
 HTML_IMAGE = {
-    "url": "https://courses.mitxonline.mit.edu/asset-v1:MITxT+3.034.3x+2T2023+type@asset+block@DALL_E_2024-10-30_15.41.00_-_A_realistic_illustration_of_a_polar_bear_standing_on_an_icy_landscape__looking_calm_and_majestic._The_background_shows_snow-covered_terrain_with_a_hin.webp",
+    "url": "https://courses.mitxonline.mit.edu/asset-v1:MITxT+3.034.3x+2T2023+type@asset+block@Polar_bear.jpg",
     "alt_text": "Ilustration of polar bear",
     "width": "200",
    
