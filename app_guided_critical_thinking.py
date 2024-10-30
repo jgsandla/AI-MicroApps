59ef20bd46c6124d91132bda76d1f3884a8a472b
jgsandla <46978608+jgsandla@users.noreply.github.com>
2024-10-30 16:39:44 -0400

    Update app_guided_critical_thinking.py

--- app_guided_critical_thinking.py
+++ app_guided_critical_thinking.py
@@ -30,33 +30,7 @@ PHASES = {
                 "type": "image",
                 "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Polar_bear_with_cub.jpg/1600px-Polar_bear_with_cub.jpg",
                 "alt": "A polar bear with its cub",
-            },
-            "identify": {
-                "type": "text_area",
-                "height": 125,
-                "label": """What is occuring in the progression shown in the image? Make sure to mention the specific condition, the yellow substance, and the risks of this condition.""",
-                "value": "The image depicts the progression of atherosclerosis, a condition in which plaque builds up inside the arteries. Atherosclerosis restricts blood flow and leads to increased risk of blod clots, heart attacks, and stroke. ",
             }
-        },
-        "no_submission": False,
-        # "phase_instructions": "The user will upload an image. Then they will describe the image. They should mention that the image is of a hand. ",
-        "user_prompt": "{identify}",
-        "allow_skip": True,
-        "scored_phase": True,
-        "rubric": """
-                1. atherosclerosis
-                    1 point - The user mentions atherosclerosis
-                    0 points - The user does not mention atherosclerosis
-                2. cholesterol/plaque
-                    1 point - The user correctly identifies that the image shows a plaque or cholesterol buildup. Either term is acceptable. 
-                    0 points - THe user does not mention plaque or cholesterol. 
-                3. risks
-                    3 point - The user mentions all relevant points: This condition restricts blood flow and increases risk of heart attack, stroke, and other cardiovascular conditions.
-                    2 point - The user mentions some of the relevant points: This condition restricts blood flow and increases risk of heart attack, stroke, and other cardiovascular conditions.
-                    1 points - THe user mentions at least one of the relevant points: This condition restricts blood flow and increases risk of heart attack, stroke, and other cardiovascular conditions.
-                    0 points - The user fails to mention any risks. 
-            """,
-        "minimum_score": 1
     },
 
 
