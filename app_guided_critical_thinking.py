2c51e86a7890802e2622b710f8c61fdfe81aea52
jgsandla <46978608+jgsandla@users.noreply.github.com>
2024-10-30 13:13:07 -0400

    Update app_guided_critical_thinking.py

--- app_guided_critical_thinking.py
+++ app_guided_critical_thinking.py
@@ -74,47 +74,6 @@ PHASES = {
 
 
  
-    "improve": {
-        "name": "Improve your Response",
-        "fields": {
-            "multimedia_principle_revised": {
-                "type": "text_area",
-                "height": 200,
-                "label": """Based on the feedback you received, please improve your response.""",
-                "label_visibility": "hidden",
-                "value": """The multimedia principle is the idea that learner's retain information better when viewing app_images that match the audio, rather than audio alone. This is because making a connection between a pictorial and a verbal representation of an item is proven to be an helpful way to store something in long term memory. You might expect that animated app_images are better than static app_images, but the research does not conclusively indicate that. Animated app_images could be more distracting and therefore less effective than static app_images. The exception is when demonstrating complex manual processes, where animations have shown to be effective. In my own work, I expect that I might try to use app_images that support the audio instead of purely decorative app_images or 'talking head' format. I'm thinking particularly of a lesson about building AI apps, where it would be helpful to show screenshots of important steps instead of just talking or writing about them. """,
-            },
-
-
-        },
-        "phase_instructions": """
-        The user will define the multimedia principle as it pertains to online education. Provide feedback for the user using the following guide:
-        - Make sure the user includes a definition of the multimedia principle
-        - Make sure the user describes when to use animation vs static illustration. 
-        - Make sure the user includes their own example of how they might use the multimedia principle in practice. 
-        If the user has improved their earlier response, then applaud them for doing so. If the user did not change their response, then express some disappointment.
-        Do not end your statement with a question. 
-        """,
-        "user_prompt": "{multimedia_principle_revised}",
-        "ai_response": True,
-        "scored_phase": True,
-        "minimum_score": 2,
-        "rubric": """
-            1. Definition
-                1 points - The user has provided an accurate definition of the multimedia principle in the context of online education. 
-                0 points - The user has not provided an accurate definition of the multimedia principle in the context of online education. 
-            2. Personal Example
-                1 points - The user has provided their own example of how they might use the multimedia principle in practice. 
-                0 points - The user has not provided their own example of how they might use the multimedia principle in practice. 
-            3. Animation vs Static
-                1 points - The user has accurately spoken to when the multimedia principle advises when to use animation (primarily to illustrate hands-on procedures or to serve an interpretive function) vs static app_images (most other educational visualizations)
-                0 points - The user has not spoken to when multimedia principle advises to use animation vs static app_images. 
-        """,
-        #"allow_revisions": True,
-        "allow_skip": False,
-        #"show_prompt": True,
-        #"read_only_prompt": False
-    },
 }
 
 PREFERRED_LLM = "gpt-4o-mini"
