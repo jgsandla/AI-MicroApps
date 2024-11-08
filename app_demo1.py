0b012a8043be62a79825c4fd83b17353fa8c3828
jgsandla <46978608+jgsandla@users.noreply.github.com>
2024-11-08 10:57:25 -0500

    Update app_demo1.py

--- app_demo1.py
+++ app_demo1.py
@@ -287,72 +287,7 @@ PHASES = {
         "allow_skip": True,
   
     },
-    "phase3": {
-        "name": "Radio Select",
-        "fields": {
-            "pluto": {
-                "type": "radio",
-                "options": ['Yes!', 'No', 'It is whatever NASA tells me it is.'],
-                "label": "Is Pluto a planet?",
-            }
-        },
-        "phase_instructions": "The user has been asked 'Is pluto a planet?' and will give their response. Comment on their response",
-        "user_prompt": "{pluto}",
-        "allow_skip": True,
-    },
-    "phase4": {
-        "name": "Dropdown Select",
-        "fields": {
-            "tech": {
-                "type": "selectbox",
-                "options": ['Mac', 'PC', 'Linux!', 'Stone Tablet'],
-                "label": "Mac or PC?",
-            }
-        },
-        "phase_instructions": "The user will tell you whether they prefer Mac, PC, Linux or provide a flippant answer about their choice of technology. Tell them a joke about whatever their choice is.",
-        "user_prompt": "{tech}",
-        "allow_skip": True,
-    },
-    "phase5": {
-        "name": "Checkbox",
-        "fields": {
-            "check_me": {
-                "type": "checkbox",
-                "label": "Check this box!",
-            }
-        },
-        "phase_instructions": "The user will tell you if they have pets or not. Comment on it.",
-        "ai_response": False,
-        "custom_response": "Thanks!",
-        "allow_skip": True,
-    },
-    "phase6": {
-        "name": "age",
-        "fields": {
-            "age": {
-                "type": "slider",
-                "min_value": 5,
-                "max_value": 100,
-                "label": "How old are you?",
-            }
-        },
-        "phase_instructions": "The user will provide their age. Say something encouraging about their age.",
-        "user_prompt": "{age}",
-        "allow_skip": True,
-    },
-    "phase7": {
-        "name": "mars",
-        "fields": {
-            "mars": {
-                "type": "number_input",
-                "step": 1,
-                "label": "In what year do you think the first human will reach step foot on Mars?",
-            }
-        },
-        "phase_instructions": "The user will predice what year the first person will step foot on Mars. React to their answer. If their answer is non-sensical, provide a sarcastic comment.",
-        "user_prompt": "{mars}",
-        "allow_skip": True,
-    }
+   
 
 }
 
