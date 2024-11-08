7e330106309b6b77109be5df9ee99dddae461bef
jgsandla <46978608+jgsandla@users.noreply.github.com>
2024-11-08 13:09:14 -0500

    Update app_demo2.py

--- app_demo2.py
+++ app_demo2.py
@@ -375,7 +375,7 @@ PHASES = {
       #              {"next_course": "Yes"},
       #              {"revise_it": "Revise it"}
       #          ]},
-      #          "prompt": "Here is what I am interested in learning about: {student_query}  Here's some information about my academic and work background: {background}.  This is how I answered the question Have you taken Calculus? {Calculus}  This is how I answered the question How would you rank your Python skills? {Python} This is how I answered the queestion  Are you interested in self-study courses? {Self_study}.  I have already taken {previous_course}, and I am looking for a recommendation as to what course to take next. "
+      #          "prompt": "Here is what I am interested in learning about: {student_query}  Here's some information about my academic and work background: {background}.  This is how I answered the question Have you taken Calculus? {Calculus}  This is how I answered the question How would you rank your Python skills? {Python} This is how I answered the queestion  Are you interested in self-study courses? {Self_study}.  I have already taken {previous_course}, and I am looking for a recommendation as to what course to take next. Do not recommend that I take {previous_course}.  Make a different recommendation "
       #      },
 
             
